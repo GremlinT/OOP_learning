b32aec7b9397f66a9e4768745d3c9d0a1816d404
GremlinT <toxin@211.ru>
2021-10-30 18:07:37 +0300

    add class Point, HorizontalLine and VerticalLine, add creation of two lines

--- Learning OOP/Program.cs
+++ Learning OOP/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Learning_OOP
 {
@@ -6,7 +10,11 @@ namespace Learning_OOP
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HorizontalLine line = new HorizontalLine(2, 20, 3, '*');
+            line.Draw();
+            VerticalLine vLine = new VerticalLine(2, 4, 10, '*');
+            vLine.Draw();
+
             Console.ReadLine();
         }
     }
